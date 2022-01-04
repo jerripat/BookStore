d4563bb475e71ace835266adda4c033400430caa
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-03 19:17:11 -0600

    updates

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -6,7 +6,7 @@ from django.db.models import Avg
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("title")
     num_books = books.count()
     avg_rating = books.aggregate(Avg("rating"))
     return render(request, "book_outlet/index.html", {
