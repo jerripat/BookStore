9d8af9f3d69c7db1fb32d095b96f536a4780ed14
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-04 15:56:14 -0600

    delete records

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,17 +1,24 @@
 from django.db import models
-from django.core.validators  import MinValueValidator,MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-    author = models.CharField(null=True,max_length=100)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     bestseller = models.BooleanField(False)
-    slug = models.SlugField(default="", blank=True, null=False,db_index=True)
-    
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-        
+
     def __str__(self):
-        return  f"{self.title} ({self.rating})"
+        return f"{self.title} ({self.rating})"
