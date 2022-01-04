022dcc1bd471c8e8af8d3a5eed8477d585c96010
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-04 12:05:23 -0600

    model update

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Book
 
+
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug",)
 #Register your models here
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
