3070118c705f3d922b5d3236a47d425bf887caae
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-09 11:42:09 -0600

    one to one

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -3,20 +3,26 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
+
 class Address(models.Model):
     street = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+        
+    class Meta:
+       verbose_name_plural   ="Address Entries"  
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
-    
-    
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True)
+
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
-    
+
     def __str__(self):
         return self.full_name()
 
@@ -25,7 +31,8 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books")
     bestseller = models.BooleanField(False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
